30bb8892bf4d3d8f104729c2df42842fce5421fd
Dayana Mariano <dayanaraquelle@gmail.com>
2025-02-13 16:00:51 -0300

    :sparkles: feat: Add Title style

--- app/components/Title/index.jsx
+++ app/components/Title/index.jsx
@@ -1,10 +1,11 @@
 import { View, Text } from "react-native"
 import React from "react"
+import styles from "./style.js"
 
 export default function Title() {
     return(
-        <View>
-            <Text>Calculadora de IMC</Text>
+        <View style={styles.boxTitle}>
+            <Text style={styles.textTitle}>Calculadora de IMC</Text>
         </View>
     )
 }
--- app/index.jsx
+++ app/index.jsx
@@ -1,13 +1,23 @@
-import { Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
 import Title from "./components/Title";
 import Main from "./components/Main";
 
 export default function Index() {
 
   return (
-    <View>
+    <View style={styles.container}>
       <Title/>
       <Main/>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#e0e5e5',
+    paddingTop: 80,
+    // alignItems: 'center',
+    // justifyContent: 'center',
+  },
+})
